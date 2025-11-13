3690d86e34c88a56c57cdbaf22caadf61d157fe6
codepraycode <preciousolusola16@gmail.com>
2025-11-13 13:30:47 +0100

    Fix lax issues for serevr

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,13 +1,15 @@
 import jwt from "jsonwebtoken"
-export const generateToken = (userId, res)=>{
-    const token = jwt.sign({userId}, process.env.JWT_TOKEN, {
-        expiresIn : "7d"
-    })
+import { isProd } from "../config/env.js";
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d", // 7 days
+    });
+
     res.cookie("jwt", token, {
-        maxAge : 7 * 24 * 60 * 60 * 1000,
-        httpOnly : true,
-        sameSite : "strict",
-        secure : process.env.NODE_ENV !== "development"
-    })
-    return token
-}   
+        httpOnly: true,           // can't be read by JS
+        secure: isProd,           // only over HTTPS in prod
+        sameSite: isProd ? "none" : "lax", // cross-site in prod, lax in dev
+        maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days
+    });
+};
